ad65a40b7830f5cfc90ebb0d2fc032ad4cbfcd1c
NurulC <nurulc@yahoo.com>
2020-12-12 01:00:56 -0500

    Update utils.js

--- asset/utils.js
+++ asset/utils.js
@@ -1,9 +1,7 @@
 
 function capitalize (value) {
-  var textArray = value.split(' ')
-  var capitalizedText = ''
-  for (var i = 0; i < textArray.length; i++) {
-    capitalizedText += textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1) + ' '
-  }
-  document.getElementById('name').innerText = capitalizedText.trim()
+  return  value.trim()
+               .split(' ')
+                  .map(s => s.charAt(0).toUpperCase() + s.slice(1))
+               .join(' ')
 }
