d229e1965fd35f24907101318f9f977f6a7821b2
nurulc <nurulc@yahoo.com>
2021-01-20 09:53:20 -0500

    start coding sample generator

--- bin/gen-template.js
+++ bin/gen-template.js
@@ -1,11 +1,11 @@
-const pkg = require('../package.json');
+//const pkg = require('../package.json');
 const fs = require('fs');
 const process = require('process');
 // check if template has been created 
-if (fs.existsSync('ref_src')) {
-    console.log('Tryit template exists');
-    process.exit(1);
-}
+// if (fs.existsSync('ref_src')) {
+//     console.log('Tryit template exists');
+//     process.exit(1);
+// }
 
 
-fs.mkdirSync('ref_src');
+// fs.mkdirSync('ref_src');
